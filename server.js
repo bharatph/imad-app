31ef422abb9a9feeefb6dfc7d0cb01b45d5ebc1d
Bharatvaj <bharatvajph@gmail.com>
2018-03-02 00:00:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,10 +53,10 @@ app.get('/ui/:lolName', function(req, res) {
   res.send(createTemplate(lols[lolName]));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/madi.png', function (req, res) {
+app.get('/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/ui/main.js', function (req, res) {
