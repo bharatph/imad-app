1b5718e99801646f921ec6eefcfef5bb1c1c685d
Bharatvaj <bharatvajph@gmail.com>
2018-03-02 00:18:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,10 @@ console.log('Loaded!');
 var thing = document.getElementById('thing');
 
 thing.onclick = function(){
+    var marginLeft = 0;
+    var interval = setInterval(function(){
+        marginLeft += 10;
+        thing.style.marginLeft = marginLeft + "px";
+    }, 200);
     thing.style.marginLeft = "100px";
 };
