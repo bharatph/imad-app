f4a1adc6a64f5bd26137862d2cc5b77e37c10551
Bharatvaj <bharatvajph@gmail.com>
2018-03-02 21:22:13 +0530

    Added counter request

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var counter = 0;
+app.get("/counter", function(req, res){
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/ui/:lolName', function(req, res) {
   var lolName = req.params.lolName;
   res.send(createTemplate(lols[lolName]));
--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,16 @@
-console.log('Loaded!');
-var thing = document.getElementById('thing');
-
-thing.onclick = function(){
-    var marginLeft = 0;
-    var interval = setInterval(function(){
-        marginLeft += 1;
-        thing.style.marginLeft = marginLeft + "px";
-    }, 50);
+//Counter node
+var counterBtn = document.getElementById("counterBtn");
+counterBtn.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var count = document.getElementById("count");
+                count.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open("GET", "http://localhost/counter", true);
+    request.send(null);
 };
